3e9f72fa1723be74eec67dda2ccb14f417e032fe
Ezio Lemes <eziolemes@gmail.com>
2022-12-21 23:19:55 -0400

    exemplo de formulario com hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [tarefas, setTarefas] = useState([
+    'Pagar a conta de luz',
+    'Estudar Reactjs'
+  ]);
+
+  function handleRegister(e) {
+    e.preventDefault();
+
+    setTarefas([...tarefas, input]);
+    setInput('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Cadastrando usuario</h1>
+
+      <form onSubmit={handleRegister}>
+        <label htmlFor="">Nome da tarefa:</label><br />
+        <input 
+          type="text" 
+          placeholder='Digite uma tarefa' 
+          value={input}
+          onChange={ (e) => setInput(e.target.value) }
+        /><br />
+
+        <button type='submit'>Registrar</button>
+      </form>
+
+      <br /><br />
+
+      <ul>
+        {tarefas.map( tarefa => (
+          <li key={tarefa}>{tarefa}</li>
+        ))}
+      </ul>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/components/Nome.js
@@ -0,0 +1,7 @@
+function Nome({ aluno, idade }) {
+  return (
+    <span>Bem-vindo: {aluno} - Idade: {idade} anos</span>
+  )
+}
+
+export default Nome;
